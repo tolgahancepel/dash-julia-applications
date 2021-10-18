0d0ff2f79623581ae2f7beb3989624ad8842143e
unknown <tolgahan.cepel@gmail.com>
2021-10-18 12:36:12 +0300

    github image updated

--- activation-functions/app.jl
+++ activation-functions/app.jl
@@ -3,7 +3,6 @@ app.jl
 """
 
 # Authors: Tolgahan Cepel <tolgahan.cepel@gmail.com>
-
 using PlotlyJS
 using Dash, DashHtmlComponents, DashCoreComponents, DashBootstrapComponents
 
@@ -12,12 +11,10 @@ app = dash(
     "https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML",
     dbc_themes.UNITED]
 )
-dcc_interval()
 
 # -----------------------------------------------------------------------------
 # Activation Functions
 # -----------------------------------------------------------------------------
-
 
 # Linear
 function linear(x)
@@ -66,7 +63,7 @@ function hardtanh(x)
     return x_new
 end
 
-# Relu
+# ReLu
 function relu(x)
     x_new=[]
     for i in x
@@ -75,6 +72,29 @@ function relu(x)
     return x_new
 end
 
+# Piece-wise linear
+function piece_wise_linear(x)
+    x_new=[]
+    for i in x
+        if (i < -1/2)
+            append!(x_new,0)
+        elseif i > -1/2 && i < 1/2
+            append!(x_new, i+1/2)
+        else
+            append!(x_new,1)
+        end
+    end
+    return x_new
+end
+
+# Rectifier Softplus
+function rectifier_softplus(x)
+    x_new=[]
+    for i in x
+        append!(x_new,log(exp(i) + 1))
+   end
+   return x_new
+end
 
 # -----------------------------------------------------------------------------
 # Card - Dropdown
@@ -94,16 +114,18 @@ card_dropdown = dbc_card(
                         (label="Sigmoid", value="sigmoid"),
                         (label="Hyperbolic Tangent", value="tanh"),
                         (label="ReLu", value="relu"),
+                        (label="Piece-wise Linear", value="piece_wise_linear"),
+                        (label="Rectifiter (Softplus)", value="rectifier_softplus"),
                     ],
                     value="",
                     multi=true
                 ),
-                
+
                 dcc_radioitems(
                     id="radio-ranges",
                     options=[
-                        (label="(-1 to +1)", value="one"),
-                        (label="(-10 to +10)", value="ten")
+                        (label="[-1 to +1]", value="one"),
+                        (label="[-10 to +10]", value="ten")
                     ],
                     value="one"
                 )
@@ -113,15 +135,20 @@ card_dropdown = dbc_card(
     ]
 )
 
+# -----------------------------------------------------------------------------
+# Card - Info
+# -----------------------------------------------------------------------------
+
 card_info = dbc_card(
     [
-        dbc_cardheader("INFO"),
+        dbc_cardheader("Information"),
         dbc_cardbody(
             [
-                html_p("Please use [-10 to 10] range for Sigmoid and Hyperbolic Tangent. Have fun!"),
+                html_p("Please use [-10 to 10] range for Sigmoid, Hyperbolic Tangent and Rectifier(Softplus)."),
                 html_a(
-                    href="#",
-                    html_img(src="assets/GitHub-Mark-Light-64px.png", width=32)
+                    href="https://github.com/tolgahancepel/dash-julia-applications/tree/main/activation-functions",
+                    html_img(src="assets/GitHub.png", width=96),
+                    target="_blank"
                 )
             ]
         )
@@ -134,7 +161,7 @@ card_info = dbc_card(
 
 card_graph = dbc_card(
     [
-        dbc_cardheader("1-Dimension Graph"),
+        dbc_cardheader("1D Graph"),
         dbc_cardbody(dcc_graph(id="graph"))
     ]
 )
@@ -148,6 +175,7 @@ app.layout = dbc_container(
         dbc_jumbotron(
             html_h3("Activation Functions in Neural Networks"),
         ),
+
         dbc_row(
             [
                 dbc_col(
@@ -180,27 +208,26 @@ callback!(
      Input("radio-ranges", "value")]
 ) do function_names, range_selected
 
+    # Assigning xaxis range
     axis_range=[]
     if range_selected=="one"
         axis_range=[-1.01,1.01]
     else
         axis_range=[-10.01,10.01]
     end
-    
 
+    # Layout
     layout=Layout(
         autosize=true,
         margin=attr(l=20, r=20, b=20, t=50),
         hovermode="closest",
         plot_bgcolor="#192233",
         paper_bgcolor="#192233",
-        # legend=dict(font=dict(size=10), orientation="h"),
-        # title="1-Dimension Graph",
         font_color ="#fff",
         xaxis_showgrid=false,
         yaxis_showgrid=false,
         showlegend=true,
-        
+        font_size=14,
         polar=attr(
             radialaxis=attr(
                 range=[0,100],
@@ -217,12 +244,6 @@ callback!(
                 color="#fff"
             ),
         ),
-        yaxis=attr(
-            title="Activation(z)",
-            range=[-1.02,1.02],
-            tickfont_size= 14,
-            tickfont_color="rgb(107, 107, 107)"
-        ),
         xaxis=attr(
             title="z",
             range=axis_range,
@@ -230,28 +251,30 @@ callback!(
             tickfont_color="rgb(107, 107, 107)",
             
         ),
-
-        font=attr(
-            size=14,
+        yaxis=attr(
+            title="Activation(z)",
+            range=[-1.02,1.02],
+            tickfont_size= 14,
+            tickfont_color="rgb(107, 107, 107)"
         )
-
     )
 
-
-
+    # Checking the selected values, returns empty figure if no selection
     if isempty(function_names)
         return plot(layout)
     else
-        x = range(-10,stop=10,length=5000)
+        x = range(-10,stop=10,length=5000)    # 
         traces=[]
         
+        # Changing datatype to Vector if only one function selected
         if typeof(function_names) !== Vector{Any}
             function_names=[function_names]
         end
-        
-
+       
+        # Iterate over selected function names
         for name in function_names
-
+            
+            # Assigning y value for each function
             if name=="linear"
                 y=linear(x)
             elseif name=="unit_step"
@@ -264,21 +287,25 @@ callback!(
                 y=hardtanh(x)
             elseif name=="relu"
                 y=relu(x)
+            elseif name=="piece_wise_linear"
+                y=piece_wise_linear(x)
+            elseif name=="rectifier_softplus"
+                y=rectifier_softplus(x)
             end
 
-
+            # Adding a new trace to traces list for each function
             push!(
                 traces,
                 scatter(
                     x=x,
                     y=y,
                     name=name,
-                    # marker_color="red"
-                    line_width=4,
+                    line_width=6,
                 )
             )
         end
 
+        # Creating plot using all the traces and layout
         fig=plot(
             [trace for trace in traces],
             layout
@@ -287,6 +314,5 @@ callback!(
         return fig
     end
 end
-
 
 run_server(app, "0.0.0.0", debug=false)
